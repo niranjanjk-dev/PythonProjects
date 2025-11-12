e7679727658a2dc3f8f8526c083a62a7ea9fea1c
NiranjanJagadeeshKumarS <s.niranjanjagadeeshkumarsnk@gmail.com>
2025-11-12 09:39:20 +0530

    Trial

--- Flask_Projects/TodoList/website/views.py
+++ Flask_Projects/TodoList/website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint , request
 from flask import render_template
 
 views= Blueprint("views", __name__)
@@ -7,7 +7,13 @@ views= Blueprint("views", __name__)
 def home():
     return "<h1>Hello</h1>"
 
-@views.route("/profile/<username>")
-def profile(username):
-    return render_template("index.html", name=username)
+@views.route("/profile",methods=["GET","POST"])
+def form_handdling():
+    if request.method == "POST":
+        title=request.form.get(Title)
+        content= request.form.get(content)
+        print(title,content)
+        return f"<h1>{title,content}</h1>"
+
+    return render_template("index.html" )
 
