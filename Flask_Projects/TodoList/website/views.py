4c31026fbb2d677eb3654cab05c0a174fb4035f4
NiranjanJagadeeshKumarS <s.niranjanjagadeeshkumarsnk@gmail.com>
2025-11-15 22:58:05 +0530

    Frontend added pages

--- Flask_Projects/TodoList/website/views.py
+++ Flask_Projects/TodoList/website/views.py
@@ -2,8 +2,12 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views.route("/gallery")
+@views.route("/home")
 def home():
+    return render_template("home.html")
+
+@views.route("/gallery")
+def gallery():
     return render_template("gallery.html")
 
 @views.route("/add-content")
@@ -13,3 +17,11 @@ def add_content():
 @views.route("/profile")
 def profile():
     return render_template("profile.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
+
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
